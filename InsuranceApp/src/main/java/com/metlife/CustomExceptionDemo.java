1bcdbf3cf8d313d92bb5d9bb6ae8eea2f07e727c
eswaribala <parameswaribala@gmail.com>
2023-09-28 13:37:49 +0530

    updated

--- InsuranceApp/src/main/java/com/metlife/CustomExceptionDemo.java
+++ InsuranceApp/src/main/java/com/metlife/CustomExceptionDemo.java
@@ -1,12 +1,28 @@
 package com.metlife;
 
+import com.metlife.exceptions.DOBException;
 import com.metlife.models.FullName;
+import com.metlife.models.PolicyHolder;
+
+import java.time.LocalDate;
 
 public class CustomExceptionDemo {
 
     public static void main(String[] args){
         FullName fullName=new FullName();
         fullName.setFirstName("Parameswari");
+
+        PolicyHolder policyHolder=new PolicyHolder();
+        try {
+            if (LocalDate.now().plusDays(10).isAfter(LocalDate.now()))
+                throw new DOBException("Date of Birth cannot be after current Date");
+            else
+                policyHolder.setDob(LocalDate.now().plusDays(10));
+            System.out.println(policyHolder.getDob().toString());
+        }
+        catch (DOBException ex){
+           System.out.println(ex.getMessage());
+        }
 
     }
 }
---
+++ InsuranceApp/src/main/java/com/metlife/exceptions/DOBException.java
@@ -0,0 +1,7 @@
+package com.metlife.exceptions;
+
+public class DOBException extends Exception{
+    public DOBException(String message) {
+        super(message);
+    }
+}
