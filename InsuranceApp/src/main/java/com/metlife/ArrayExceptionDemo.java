1f6119ee8cd2ef0ededf890feb68e4aac683942d
eswaribala <parameswaribala@gmail.com>
2023-09-28 13:02:56 +0530

    updated

--- InsuranceApp/src/main/java/com/metlife/ArrayExceptionDemo.java
+++ InsuranceApp/src/main/java/com/metlife/ArrayExceptionDemo.java
@@ -12,28 +12,25 @@ import java.util.Scanner;
 public class ArrayExceptionDemo {
 
     public static void main(String[] args){
-     Scanner scanner=new Scanner(System.in);
-     System.out.println("Enter index");
-     int index=scanner.nextInt();
-     scanner.nextLine();
-        try {
-            System.out.println(args[index]); //unchecked
-        }
-        catch (ArrayIndexOutOfBoundsException ex){
-            System.out.println(ex.getMessage());
-        }
         int x=10;
         int y=0;
-        System.out.println("Enter y value");
-        y=scanner.nextInt();
-        scanner.nextLine();
+         Scanner scanner=new Scanner(System.in);
+         System.out.println("Enter index");
+         int index=scanner.nextInt();
+         scanner.nextLine();
         try {
+            System.out.println(args[index]); //unchecked
+            System.out.println("Enter y value");
+            y=scanner.nextInt();
+            scanner.nextLine();
             System.out.println(x / y); //unchecked
-
         }
-        catch (ArithmeticException ex){
+        catch (ArrayIndexOutOfBoundsException |ArithmeticException  ex){
             System.out.println(ex.getMessage());
         }
+
+
+
 
     }
 }
