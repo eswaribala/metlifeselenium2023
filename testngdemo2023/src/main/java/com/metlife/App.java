d4a19e24561a253a793dac61e15323c9a09e7776
eswaribala <parameswaribala@gmail.com>
2023-10-02 11:19:01 +0530

    updated

--- testngdemo2023/src/main/java/com/metlife/App.java
+++ testngdemo2023/src/main/java/com/metlife/App.java
@@ -1,13 +1,41 @@
 package com.metlife;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+
+    private WebDriver webDriver;
+    private String baseUrl;
+
+    @BeforeClass
+    public void browserSetup(){
+
+        webDriver=new ChromeDriver();
+        baseUrl="https://www.travelsupermarket.com";
+
     }
+
+   @Test
+    public void testTitle(){
+
+      webDriver.get(baseUrl);
+       assertEquals("Compare Travel Deals | TravelSupermarket",webDriver.getTitle());
+
+    }
+
+
+
+
 }
