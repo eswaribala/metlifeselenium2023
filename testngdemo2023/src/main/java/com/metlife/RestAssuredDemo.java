1b3be973b32b1778f5f56cf799b2eb8f0c08d78e
parameswari.bala <parameswaribala@gmail.com>
2023-10-11 13:40:13 +0530

    updated

--- testngdemo2023/src/main/java/com/metlife/RestAssuredDemo.java
+++ testngdemo2023/src/main/java/com/metlife/RestAssuredDemo.java
@@ -2,6 +2,7 @@ package com.metlife;
 
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
 
 public class RestAssuredDemo {
 
@@ -11,6 +12,18 @@ public class RestAssuredDemo {
     public void testRestCountriesStatusCodeToBe200(){
 
         given().get("https://restcountries.com/v2/all").then().statusCode(200);
+    }
+
+
+    @Test
+    public void testRestUsersCountTobe10(){
+
+        given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/users")
+                .then()
+                .assertThat()
+                .body("id",hasSize(10));
     }
 
 
