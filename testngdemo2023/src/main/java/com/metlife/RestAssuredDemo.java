9a668ad943eb09f4a31b3dc309feab7851fd6cca
parameswari.bala <parameswaribala@gmail.com>
2023-10-12 11:53:37 +0530

    updated

--- testngdemo2023/src/main/java/com/metlife/RestAssuredDemo.java
+++ testngdemo2023/src/main/java/com/metlife/RestAssuredDemo.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.metlife.models.PolicyHolder;
 import io.restassured.http.ContentType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Date;
@@ -63,26 +64,43 @@ public class RestAssuredDemo {
     public void testAddPolicyHolder() throws JsonProcessingException {
 
         PolicyHolder policyHolder=new PolicyHolder();
-        policyHolder.setPolicyNo(101);
-        policyHolder.setFirstName("Sanjay");
-        policyHolder.setLastName("kumar");
+        policyHolder.setPolicyNo(102);
+        policyHolder.setFirstName("Anjana");
+        policyHolder.setLastName("Singh");
         policyHolder.setDob(new Date(1900+new Random().nextInt(120),
                 1+new Random().nextInt(10),1+new Random().nextInt(25) ));
-        policyHolder.setMobileNo(12435356667L);
+        policyHolder.setMobileNo(1234567890L);
         String newPolicyHolderXml = new XmlMapper().writeValueAsString(policyHolder);
 
-        given()
+       String mobileNo= given()
                 .body(newPolicyHolderXml)
                 .contentType("application/xml")
                 .accept(ContentType.XML)
                 .when()
                 .post("http://localhost:7072/policyholders/v1.0/")
                 .then()
-                .statusCode(202);
+                .statusCode(202)
+                .extract()
+                .path("PolicyHolder.mobileNo");
+        Assert.assertTrue(mobileNo.length()==10, "String message");
 
     }
 
+   /* @Test
+    public void testGetPolicyHolders() throws JsonProcessingException {
 
+
+
+        given()
+
+                .contentType("application/xml")
+                .accept(ContentType.XML)
+                .when()
+                .get("http://localhost:7072/policyholders/v1.0/")
+                .then()
+                .statusCode(202);
+
+    }*/
 
 
 
