1e50ba715ac32f2543be4aa2b54ff8147c50a59f
parameswari.bala <parameswaribala@gmail.com>
2023-10-12 13:11:29 +0530

    updated

--- metlifemultimodule/insuranceapp/homeinsurance/src/main/java/com/metlife/HomeInsuranceApp.java
+++ metlifemultimodule/insuranceapp/homeinsurance/src/main/java/com/metlife/HomeInsuranceApp.java
@@ -1,6 +1,8 @@
 package com.metlife;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,18 +14,23 @@ import org.testng.annotations.Test;
 public class HomeInsuranceApp
 {
    private WebDriver webDriver;
-
+   private WebElement webElement;
    @BeforeClass
    public void setup(){
 
        webDriver=new ChromeDriver();
+       webDriver.get("https://www.rediffmail.com");
 
    }
 
     @Test(retryAnalyzer = com.metlife.listeners.RetryListener.class)
-    public void testGoogle(){
-        webDriver.get("https://www.rediffmail123.com");
-        webDriver.quit();
+    public void testGoogle() throws InterruptedException {
+       webDriver.navigate().to("https://www.rediffmail.com");
+       webElement=webDriver.findElement(By.xpath("//*[@id=\"OAS_subsection1\"]"));
+
+       Thread.sleep(1000);
+       // webDriver.get("https://www.rediffmail123.com");
+        //webDriver.quit();
     }
 
 }
