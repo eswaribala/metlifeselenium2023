aa7b1bc208769d56ab4ea1ca748142458ba346c2
parameswari.bala <parameswaribala@gmail.com>
2023-10-13 11:22:19 +0530

    updated

--- cucumberjava/cucumbersbdemo/src/test/java/com/metlife/cucumbersbdemo/cucumber/stepdefs/CanadaPhoneNumberTest.java
+++ cucumberjava/cucumbersbdemo/src/test/java/com/metlife/cucumbersbdemo/cucumber/stepdefs/CanadaPhoneNumberTest.java
@@ -3,6 +3,7 @@ package com.metlife.cucumbersbdemo.cucumber.stepdefs;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,6 +11,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 import java.time.Duration;
 
+@CucumberContextConfiguration
 public class CanadaPhoneNumberTest {
 
     private WebDriver webDriver;
